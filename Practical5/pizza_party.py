a7ea58dd536189e0dffaf1620bb31e2ac575fae5
rustylakeparadox <101188492+rustylakeparadox@users.noreply.github.com>
2022-03-15 18:31:26 +0800

    Create pizza_party.py
    correct some errors

---
+++ Practical5/pizza_party.py
@@ -0,0 +1,12 @@
+p = 1
+# the initial value for p should be 1
+n = 1
+# the initial value for n should be 1
+while p < 64:
+# set a loop and make it stop once p reaches 64
+  n = n + 1
+  # increase n each time 
+  p = (n * n + n + 2)/2
+  # import the calculation formula of p
+  
+  print("the	total	number	of pieces	of	pizza is" + str(p))
