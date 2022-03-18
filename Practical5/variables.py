382644eeb08c8b110eaa97e6260ef3a27d4636fb
rustylakeparadox <101188492+rustylakeparadox@users.noreply.github.com>
2022-03-18 15:39:04 +0800

    Update variables.py
    add boolean values

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -1,7 +1,19 @@
+3.1 some simple math
 a = 19,245,301
 b = 4,218,520	
 c = 271
-d = b - c = 4218249
-e = a - b = 15026781
-d < e
-the rate	of	new	cases	was greater	in	2021
+# give the value to the parameters
+d = b - c 
+e = a - b 
+# calculate the dependent variables
+if d < e:
+  print(the rate of new	cases	was greater	in 2021)
+else:
+  print(the rate of new cases was greater in 2020)
+# output to answer the question
+
+3.2 Booleans
+X = input("X =")
+Y = input("Y =")
+W = X and Y
+print (W)
