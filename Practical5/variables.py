68e91875b0b5d862fda3987fe81ed6b83e4e3a3d
rustylakeparadox <101188492+rustylakeparadox@users.noreply.github.com>
2022-03-21 15:00:07 +0800

    Update variables.py
    add  some hints about the inputs of X and Y

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -1,4 +1,4 @@
-3.1 some simple math
+# 3.1 some simple math
 a = 19,245,301
 b = 4,218,520	
 c = 271
@@ -12,9 +12,9 @@ else:
   print(the rate of new cases was greater in 2020)
 # output to answer the question
 
-3.2 Booleans
-X = input("X =")
-Y = input("Y =")
+# 3.2 Booleans
+X = input("X = (TRUE/FALSE)")
+Y = input("Y = (TRUE/FALSE)")
 # input 2 boolean values X and Y
 W = X and Y
 # set the boolean value of W
