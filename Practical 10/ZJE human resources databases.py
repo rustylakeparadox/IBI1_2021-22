44166ccd4609ad565ef6b535359431d99e08f797
rustylakeparadox <101188492+rustylakeparadox@users.noreply.github.com>
2022-04-20 18:00:32 +0800

    Add files via upload

---
+++ Practical 10/ZJE human resources databases.py
@@ -0,0 +1,12 @@
+class staff (object):
+    def __init__(self,x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+#given an example
+a = staff('Chen Yanhui', 'International Campus', 'administration')
+print(a.x)
+print(a.y)
+print(a.z)
