abc61c47ef5d0278fcb13bfe4239a284db3d568b
rustylakeparadox <101188492+rustylakeparadox@users.noreply.github.com>
2022-04-27 17:28:49 +0800

    Update ZJE human resources databases.py
    add some pseudocodes

--- Practical 10/ZJE human resources databases.py
+++ Practical 10/ZJE human resources databases.py
@@ -1,12 +1,16 @@
-class staff (object):
-    def __init__(self,x,y,z):
-        self.x = x
+class staff (object):      # create a class
+    def __init__(self,x,y,z):     # create a function with the specific viriable "self"
+    # define the viarables
+        self.x = x               
         self.y = y
         self.z = z
 
 
-#given an example
-a = staff('Chen Yanhui', 'International Campus', 'administration')
+# give an example
+a = staff('Chen Yanhui', 'International Campus', 'administration')  # call the class
 print(a.x)
+## Chen Yanhui
 print(a.y)
+## International Campus
 print(a.z)
+## administration
