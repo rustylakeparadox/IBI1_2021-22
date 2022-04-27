d574f1cf6572c8f3ba8b91e5a3ae950d5e9da5a3
rustylakeparadox <101188492+rustylakeparadox@users.noreply.github.com>
2022-04-27 16:47:44 +0800

    Update fragment_counter.py
    add some pseudocodes

--- Practical 8/fragment_counter.py
+++ Practical 8/fragment_counter.py
@@ -1,4 +1,7 @@
 import re
+# input the sequences
 seq = 'ATGCAATCGACTACGATCAATCGAGGGCC'
+# count the number of the fragments that can be cut by the enzyme
 fragment = seq.count("GAATC") + 1
+# print out the answer
 print("the number of fragments is " + str(fragment))
