c941b64b10bad4df2eacaef48c093af1764c7194
rustylakeparadox <101188492+rustylakeparadox@users.noreply.github.com>
2022-04-27 17:14:09 +0800

    Update count_cut_genes.py
    add some psuedocodes

--- Practical 8/count_cut_genes.py
+++ Practical 8/count_cut_genes.py
@@ -1,54 +1,54 @@
+# input the file name.
 filename=input()
 
-#Now input the file name.
 # I used the file 'cut_genes.txt' in the working directory to test
 
-#First, open the file and store the data.
+# open the file and store the data.
 with open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa') as fa:
-#The data will be stored in the dict "fa_dict".
+# The data will be stored in the dictionary "fa_dict".
     fa_dict = {}
     for line in fa:
-        # Remove end line breaks "\n".
+        # Remove line breaks "\n" at the end 
         line = line.replace('\n','')
         if line.startswith('>'):
-            # Remove ">" at the beginning.
+            # Remove ">" at the beginning
             seq_name = line[1:]
             fa_dict[seq_name] = ''
         else:
-            # Remove end newline and join multiline sequences
+            # Remove the line break at the end and join multiline sequences together
             fa_dict[seq_name] += line.replace('\n','')
 
 
-#Find out sequence in the fa_dict and put the sequence and relative information into another dictionary.
+# find out the sequence that can be cut by the enzyme and put the it with its relative information into another dictionary.
 filtered_dict={}
 for (key,value) in zip(fa_dict.keys(), fa_dict.values()):
     if value.find('GAATTC')>=0:
         filtered_dict[key]=value
 
 
-#Turn the dictionary into strings.
+# swift the dictionary to strings
 res = str(filtered_dict)
 import re
-#Use re to process.
-#Filter the name and sequence in the string. 
-#The criteria is based on the text features.
+# use re to process.
+# filter the name and sequence in the string 
+# the criteria is based on the text features
 name = re.findall(r'gene:(\S+)', res)
 seq = re.findall(r'[A-Z]+GAATTC[A-Z]+', res)
-#The output will be lists.
+# the output will be lists
 
 
-# Calculate fragment number and store it in a list.
+# calculate the number of fragments and store it in a list.
 frag=[]
 for x in seq:
     ment=x.count('GAATTC')+1
     frag.append(ment)
 
-#Create the file.
+# create the file
 file = open(filename,'w')
 for i in range(0,len(seq)):
-# Store information in an 'int' first.
+# store the information in an 'int' first.
     line1=name[i]
-# Variable 'int' must be turned into a 'str' to be written in the file.
+# 'int' must be turned into a 'str' to be written in the file.
     line1=str(line1)
     line2=frag[i]
     line2=str(line2)
