57a5f6e1a2e68290303e2f4036a1ae7299af0fae
rustylakeparadox <101188492+rustylakeparadox@users.noreply.github.com>
2022-04-27 17:25:39 +0800

    Update Chocolate_bar_affordability.py
    add some pseudocodes and simplify the codes

--- Practical 10/Chocolate_bar_affordability.py
+++ Practical 10/Chocolate_bar_affordability.py
@@ -1,11 +1,12 @@
-def caculate_bar():
-   x = int(input('the total money is '))
+def caculate_bar():   # create a function
+   # input the variable x, y and swift them to strings
+   x = int(input('the total money is '))    
    y = int(input('the price is '))
-   left_over = x % y
-   number = (x-left_over)/y
-   return left_over, number
+   left_over = x % y          # caculate the left over money
+   number = (x-left_over)/y   # caculate number of chocolate bars
+   return left_over, number    # return the outcomes
 
-#given an example
-caculate_bar()
-#input x = 100, y = 7
+# give an example
+caculate_bar()  # call the function
+# input x = 100, y = 7
 ## (2,14.0)
