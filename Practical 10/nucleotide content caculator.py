57d9b6ab3c38c5a94d8fa4ee834e2c05bec993bd
rustylakeparadox <101188492+rustylakeparadox@users.noreply.github.com>
2022-04-27 17:36:05 +0800

    Update nucleotide content caculator.py
    add some pseudocodes

--- Practical 10/nucleotide content caculator.py
+++ Practical 10/nucleotide content caculator.py
@@ -1,13 +1,15 @@
-def Nucleotide_ontent_calculator():
-    genes = input("the single strand is ")
-    genes = genes.swapcase()
-    total = len(genes)
-    pc_A = genes.count("A")/total
+def Nucleotide_ontent_calculator():     # create a function
+    genes = input("the single strand is ")     # imput the viriable "genes"
+    genes = genes.swapcase()                   # swift all the strings into capital letters
+    total = len(genes)                         # count the number of the nucleotides
+    # caculate the percentage of each kind of nucleotide seperately
+    pc_A = genes.count("A")/total               
     pc_G = genes.count("G")/total
     pc_C = genes.count("C")/total
     pc_T = genes.count("T")/total
-    return pc_A, pc_G, pc_C, pc_T
+    return pc_A, pc_G, pc_C, pc_T               # return the outcomes
 
-Nucleotide_ontent_calculator()
+# give an example
+Nucleotide_ontent_calculator()      # call the function
 #input 'acGTAgT'
 ##(0.14285714285714285, 0.14285714285714285, 0.14285714285714285, 0.0)
