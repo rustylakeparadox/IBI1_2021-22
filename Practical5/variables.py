15d9c383be19ae3ef5abf97c2085e61ec89d92c3
rustylakeparadox <101188492+rustylakeparadox@users.noreply.github.com>
2022-05-25 03:11:16 +0800

    Update variables.py
    correct some mistakes, such as adding quotation marks for strings.

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -1,20 +1,20 @@
 # 3.1 some simple math
-a = 19,245,301
-b = 4,218,520	
+a = 19245301
+b = 4218520	
 c = 271
 # give the value to the parameters
 d = b - c 
 e = a - b 
 # calculate the dependent variables
 if d < e:
-  print("the rate of new	cases	was greater	in 2021")
+  print("the rate of new cases was greater in 2021")
 else:
   print("the rate of new cases was greater in 2020")
 # output to answer the question
 
 # 3.2 Booleans
-X = input("X = (TRUE/FALSE)")
-Y = input("Y = (TRUE/FALSE)")
+X = input("X = (True/False)")
+Y = input("Y = (True/False)")
 # input 2 boolean values X and Y
 W = X and Y
 # set the boolean value of W
