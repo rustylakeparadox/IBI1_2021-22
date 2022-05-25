9a3e0d5dec59275c9b8155dba4030787273bbc8e
rustylakeparadox <101188492+rustylakeparadox@users.noreply.github.com>
2022-05-25 10:49:35 +0800

    Create Allignment.py
    add the printout of identical amino acids and the calculation of BLOSUM score

--- Practical 11/Allignment.py
+++ Practical 11/Allignment.py
@@ -1,4 +1,3 @@
-import re
 #read the 3 given .fa files
 with open("DLX5_human.fa") as fa:
     human = fa.read()
@@ -6,85 +5,77 @@ with open("DLX5_mouse.fa") as fa:
     mouse = fa.read()
 with open("RandomSeq(1).fa") as fa:
     random = fa.read()
-
-# extract the amino acid sequence
-human_re = re.findall(r'\n(\S+)', human)
-human_re = str(human_re)
-seq_human=[]
-for line in human_re:
-    line= line.replace('[','')
-    if not line.startswith(']'):
-        seq_human += line
-
-# delete useless punctuation
-del seq_human[0]
-del seq_human[-1]
-
-# the same operation for the mouse and the random sequences
-mouse_re = re.findall(r'\n(\S+)', mouse)
-mouse_re = str(mouse_re)
-
-seq_mouse=[]
-for line in mouse_re:
-    line= line.replace('[','')
-    if not line.startswith(']'):
-        seq_mouse += line
-
-del seq_mouse[0]
-del seq_mouse[-1]
-
-random_re = re.findall(r'\n(\S+)', random)
-random_re = str(random_re)
-
-seq_random=[]
-for line in random_re:
-    line= line.replace('[','')
-    if not line.startswith(']'):
-        seq_random += line
-
-del seq_random[0]
-del seq_random[-1]
-
+    
 # set the initial value of edit_distance to be zero
-edit_distance_1 = 0
-for i in range(len(seq_mouse)):  #compare each amino acid
-    if seq_mouse[i]!=seq_human[i]:
-      edit_distance_1 += 1     #add a score 1 if amino acids are different
+edit_distance = 0
+for i in range(len(human)):  #compare each amino acid
+    if human[i]!=mouse[i]:
+      edit_distance += 1     #add a score 1 if amino acids are different
     
-#culcalate the percentage of identical amino acid in the comparison
-pc1= edit_distance_1/len(seq_mouse)    
-pc1= (1-pc1)*100
-
 # print out the answer
-print ('The number of different amino acids between human and mouse is',edit_distance_1)
-print("the percentage of identical amino acids between human and mouse is " + str(pc1) + "%")
+print("the edit distance between human and mouse sequences is " + str(edit_distance))
 
 # reset the value of edit)distance to be zero
-edit_distance_2 = 0
+edit_distance = 0
 # repeat the distance calculation the same as before
-for i in range(len(seq_human)): 
-    if seq_human[i]!=seq_random[i]:
-      edit_distance_2 += 1
-#culcalate the percentage of identical amino acid in the comparison
-pc2= edit_distance_2/len(seq_human)    
-pc2= (1-pc2)*100
-
+for i in range(len(random)): 
+    if random[i]!=human[i]:
+      edit_distance += 1
 # print out the answer
-print ('The number of different amino acids between human and random is',edit_distance_2)
-print("the percentage of identical amino acids between human and random is " + str(pc2) + "%")
-
+print("the edit distance between human and random sequences is " + str(edit_distance))
 
 # reset the value of edit)distance to be zero
-edit_distance_3 = 0
+edit_distance = 0
 # repeat the distance calculation the same as before
-for i in range(len(seq_random)): 
-    if seq_random[i]!=seq_mouse[i]:
-      edit_distance_3 += 1
-    
-#culcalate the percentage of identical amino acid in the comparison
-pc3= edit_distance_3/len(seq_random)    
-pc3= (1-pc3)*100
+for i in range(len(random)): 
+    if random[i]!=mouse[i]:
+      edit_distance += 1
+# print out the answer
+print("the edit distance between mouse and random sequences is " + str(edit_distance))
+
+# input amino acid sequence and BLOSUM62 score matrix
+amino_acid=['A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V','B','Z','X','*']
+BLOSUM62 = [
+            [ 4, -1, -2, -2,  0, -1, -1,  0, -2, -1, -1, -1, -1, -2, -1,  1,  0, -3, -2,  0, -2, -1,  0, -4],
+            [-1,  5,  0, -2, -3,  1,  0, -2,  0, -3, -2,  2, -1, -3, -2, -1, -1, -3, -2, -3, -1,  0, -1, -4],
+            [-2,  0,  6,  1, -3,  0,  0,  0,  1, -3, -3,  0, -2, -3, -2,  1,  0, -4, -2, -3,  3,  0, -1, -4],
+            [-2, -2,  1,  6, -3,  0,  2, -1, -1, -3, -4, -1, -3, -3, -1,  0, -1, -4, -3, -3,  4,  1, -1, -4],
+            [ 0, -3, -3, -3,  9, -3, -4, -3, -3, -1, -1, -3, -1, -2, -3, -1, -1, -2, -2, -1, -3, -3, -2, -4],
+            [-1,  1,  0,  0, -3,  5,  2, -2,  0, -3, -2,  1,  0, -3, -1,  0, -1, -2, -1, -2,  0,  3, -1, -4],
+            [-1,  0,  0,  2, -4,  2,  5, -2,  0, -3, -3,  1, -2, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1, -4],
+            [ 0, -2,  0, -1, -3, -2, -2,  6, -2, -4, -4, -2, -3, -3, -2,  0, -2, -2, -3, -3, -1, -2, -1, -4],
+            [-2,  0,  1, -1, -3,  0,  0, -2,  8, -3, -3, -1, -2, -1, -2, -1, -2, -2,  2, -3,  0,  0, -1, -4],
+            [-1, -3, -3, -3, -1, -3, -3, -4, -3,  4,  2, -3,  1,  0, -3, -2, -1, -3, -1,  3, -3, -3, -1, -4],
+            [-1, -2, -3, -4, -1, -2, -3, -4, -3,  2,  4, -2,  2,  0, -3, -2, -1, -2, -1,  1, -4, -3, -1, -4],
+            [-1,  2,  0, -1, -3,  1,  1, -2, -1, -3, -2,  5, -1, -3, -1,  0, -1, -3, -2, -2,  0,  1, -1, -4],
+            [-1, -1, -2, -3, -1,  0, -2, -3, -2,  1,  2, -1,  5,  0, -2, -1, -1, -1, -1,  1, -3, -1, -1, -4],
+            [-2, -3, -3, -3, -2, -3, -3, -3, -1,  0,  0, -3,  0,  6, -4, -2, -2,  1,  3, -1, -3, -3, -1, -4],
+            [-1, -2, -2, -1, -3, -1, -1, -2, -2, -3, -3, -1, -2, -4,  7, -1, -1, -4, -3, -2, -2, -1, -2, -4],
+            [ 1, -1,  1,  0, -1,  0,  0,  0, -1, -2, -2,  0, -1, -2, -1,  4,  1, -3, -2, -2,  0,  0,  0, -4],
+            [ 0, -1,  0, -1, -1, -1, -1, -2, -2, -1, -1, -1, -1, -2, -1,  1,  5, -2, -2,  0, -1, -1,  0, -4],
+            [-3, -3, -4, -4, -2, -2, -3, -2, -2, -3, -2, -3, -1,  1, -4, -3, -2, 11,  2, -3, -4, -3, -2, -4],
+            [-2, -2, -2, -3, -2, -1, -2, -3,  2, -1, -1, -2, -1,  3, -3, -2, -2,  2,  7, -1, -3, -2, -1, -4],
+            [ 0, -3, -3, -3, -1, -2, -2, -3, -3,  3,  1, -2,  1, -1, -2, -2,  0, -3, -1,  4, -3, -2, -1, -4],
+            [-2, -1,  3,  4, -3,  0,  1, -1,  0, -3, -4,  0, -3, -3, -2,  0, -1, -4, -3, -3,  4,  1, -1, -4],
+            [-1,  0,  0,  1, -3,  3,  4, -2,  0, -3, -3,  1, -1, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1, -4],
+            [ 0, -1, -1, -1, -2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -2,  0,  0, -2, -1, -1, -1, -1, -1, -4],
+            [-4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4,  1],]
+
+# write a function which calculates BLOSUM score
+def BLOSUM(a1,a2):
+    score=0
+    for i in range(len(seq_human)):
+        index1=amino_acid.index(a1[i])
+        index2=amino_acid.index(a2[i])
+        score=score+BLOSUM62[index1][index2]   
+    return score
+
+# use the function to calculate
+score_1=BLOSUM(seq_mouse,seq_human)
+score_2=BLOSUM(seq_human,seq_random)
+score_3=BLOSUM(seq_mouse,seq_random)
 
 # print out the answer
-print ('The number of different amino acids between mouse and random is',edit_distance_3)
-print("the percentage of identical amino acids between mouse and random is " + str(pc3) + "%")
+print('The BLOSUM score between human and mouse is',score_1)
+print('The BLOSUM score between human and random is',score_2)
+print('The BLOSUM score between mouse and random is:',score_3)
